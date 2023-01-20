a625cbecc768674eaa09aa3c6aed92de450ba312
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-01-20 19:07:36 +0000

    Fixed bug: removed the bootstrap btn class to diplay the textarea properly

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
-  let $currentTime = $("#currentDay");
+  let $currentDate = $("#currentDay");
+  let $currentTime = moment().format("h:mm:ss A");
+  let $currentHour = moment().format("h A");
 
   setInterval(() => {
     let time = moment().format("dddd, MMM Do, h:mm:ss A");
-    $currentTime.text(time);
+    $currentDate.text(time);
   }, 1000);
 
   let businessHours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
@@ -12,14 +14,14 @@ $(document).ready(function () {
     $timeBlockContainerEl.append(
       `
       <div class="row">
-        <p class="col hour time-block">${hour}${index >= 3 ? "PM" : "AM"}</p>
+        <div class="col hour">${hour}${index >= 3 ? "PM" : "AM"}</div>
         <textarea
           class="col-10"
           name="description"
           id="description"
         ></textarea>
-        <button class="col btn saveBtn" type="button">
-          <i class="fas fa-save"></i>
+        <button class="col saveBtn text-center" type="button">
+          <i class="fas margin-auto d-block fa-save"></i>
         </button>
       </div>
       `
