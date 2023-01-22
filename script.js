abd698d7db581dc7f0cd637c63c9183cb4e36e43
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-01-22 14:36:32 +0000

    Add event listener to save textarea content
    Used jquery onclick event listener to save the content of the textarea to local storage - using the data-time attr as the key and the content as the value.
    
    Also added pseudocode of how I'll be implementing the colour code of the time blocks.

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
   let $currentDate = $("#currentDay");
   let $currentTime = moment().format("h:mm:ss A");
   let $currentHour = moment().format("h A");
+  console.log($currentHour);
+  console.log($currentTime);
 
   setInterval(() => {
     let time = moment().format("dddd, MMM Do, h:mm:ss A");
     $currentDate.text(time);
   }, 1000);
+
+  // Todo: Get the items from the local storage at every refresh
 
   let businessHours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
   let $timeBlockContainerEl = $(".container");
@@ -20,11 +24,29 @@ $(document).ready(function () {
           name="description"
           id="description"
         ></textarea>
-        <button class="col saveBtn text-center" type="button">
+        <button class="col saveBtn text-center" id="save-btn" data-time="${hour}${
+        index >= 3 ? "PM" : "AM"
+      }">
           <i class="fas margin-auto d-block fa-save"></i>
         </button>
       </div>
       `
     );
   });
+
+  // Save the textarea content to local storage when the btn is pressed
+  $("button").on("click", function (e) {
+    // Get the content of the text area
+    let $textAreaValue = $(e.target).siblings("textarea").val();
+    // Save textarea content to local storage using data-time as key
+    localStorage.setItem(`${$(e.target).attr("data-time")}`, $textAreaValue);
+  });
 });
+
+// if (currentHour === textareaHour) {
+//   $("textarea").addClass("present");
+// } else if (currentHour > textareaHour) {
+//   $("textarea").addClass("past");
+// } else {
+//   $("textarea").addClass("future");
+// }
